e2b635c2d8d0672ed121daa7e226230bfee5f1b4
Wojciech Kurcab <wojciech@kurcab.com>
2020-12-22 14:44:24 +0100

    first commit

---
+++ src/main/java/com/kurcab/crud/drills/DrillsApplication.java
@@ -0,0 +1,13 @@
+package com.kurcab.crud.drills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrillsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrillsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kurcab/crud/drills/DrillsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurcab.crud.drills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrillsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
